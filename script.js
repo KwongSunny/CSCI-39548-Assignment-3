a8e0e203f87c691e9d699882440d636d816c64c8
Sunny Kwong <sunnykwong91500@gmail.com>
2022-03-21 18:15:27 -0400

    adding starter files

--- script.js
+++ script.js
@@ -7,54 +7,63 @@ let colorSelected;
 function addR() {
     numRows++;
 
-    if(numCols > 0 && numRows > 0){
-        let table = document.getElementById('grid');
+    let table = document.getElementById('grid');
 
-        let row = table.insertRow(-1);
+    let row = table.insertRow(-1);
+    for(let i = 0; i < numCols; i++){
+        let cell = row.insertCell(-1);
+        cell.onclick = (e) => {e.target.style.backgroundColor = colorSelected}
     }
+    
 }
 
 // Add a column
 function addC() {
     numCols++;
 
-    if(numCols > 0 && numRows > 0){
-        let rows = document.getElementById('grid').rows;
-        console.log(rows[0]);
+    let rows = document.getElementById('grid').rows;
 
-        for(let i = 0; i < numCols; i++){
-
-        }
+    for(let i = 0; i < numRows; i++){
+        let cell = rows[i].insertCell(-1);
+        cell.onclick = (e) => {e.target.style.backgroundColor = colorSelected}
     }
 }
 
 // Remove a row
 function removeR() {
     if(numRows > 0) numRows--;
+    let table = document.getElementById('grid');
+
+    table.deleteRow(-1);
 }
 
 // Remove a column
 function removeC() {
     if(numCols > 0) numCols--;
+
+    let rows = document.getElementById('grid').rows;
+
+    for(let i = 0; i < numRows; i++){
+        rows[i].deleteCell(-1);
+    }
 }
 
 // Set global variable for selected color
 function selectColor(){
-    colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
+
 }
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+
 }
