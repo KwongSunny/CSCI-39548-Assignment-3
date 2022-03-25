b6084b7fbd8c87a37be7b50bbfbf0dd393af0bf5
Sunny Kwong <sunnykwong91500@gmail.com>
2022-03-25 17:21:40 -0400

    empty script.js

--- script.js
+++ script.js
@@ -5,52 +5,26 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    numRows++;
 
-    let table = document.getElementById('grid');
-
-    let row = table.insertRow(-1);
-    for(let i = 0; i < numCols; i++){
-        let cell = row.insertCell(-1);
-        cell.onclick = (e) => {e.target.style.backgroundColor = colorSelected}
-    }
-    
 }
 
 // Add a column
 function addC() {
-    numCols++;
 
-    let rows = document.getElementById('grid').rows;
-
-    for(let i = 0; i < numRows; i++){
-        let cell = rows[i].insertCell(-1);
-        cell.onclick = (e) => {e.target.style.backgroundColor = colorSelected}
-    }
 }
 
 // Remove a row
 function removeR() {
-    if(numRows > 0) numRows--;
-    let table = document.getElementById('grid');
 
-    table.deleteRow(-1);
 }
 
 // Remove a column
 function removeC() {
-    if(numCols > 0) numCols--;
 
-    let rows = document.getElementById('grid').rows;
-
-    for(let i = 0; i < numRows; i++){
-        rows[i].deleteCell(-1);
-    }
 }
 
 // Set global variable for selected color
 function selectColor(){
-
 }
 
 // Fill all uncolored cells
